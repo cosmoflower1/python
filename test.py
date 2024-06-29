52bfe4010673a88a0af2b1a54e5e65b3a7ad9f02
cosmoflower1 <culturecxrner@gmail.com>
2024-06-29 15:02:46 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+# x = input('введите x: ')
+# y = input('введите y: ')
+# print(x + y)
+
+a = int(input('введите a: '))
+b = int(input('введите b: '))
+# c = a + b
+# print(c)
+c = a - b # вычитание
+print('лох =', c)
+
+d = a / b # деление
+print("a / b =" , d)
+e = a * b # умножение 
+print("a * b =" , e)
